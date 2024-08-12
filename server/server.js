fce676bc48bd98eebaa5b78a6e56150808bd6efb
Md Saquib Ansari <saquibchiksi786@gmail.com>
2024-08-12 15:10:05 +0530

    first commit

--- server/server.js
+++ server/server.js
@@ -13,3 +13,14 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+const path = require('path');
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
