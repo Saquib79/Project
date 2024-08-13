b86ef2dd9f59fe7b4a4253f432b8e62019e6e8e5
Md Saquib Ansari <saquibchiksi786@gmail.com>
2024-08-13 14:15:23 +0530

    commit

--- server/server.js
+++ server/server.js
@@ -27,11 +27,7 @@ app.use(express.static(path.join(__dirname, '..client/build')));
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
 app.get('*', (req, res) => {
-<<<<<<< HEAD
   res.sendFile(path.resolve(__dirname, '..client/build', 'index.html'));
-=======
-  res.sendFile(path.join(__dirname, '..client/build', 'index.html'));
->>>>>>> 1976160c96f3841c87a37dc6af2df979023d3306
 });
 
 app.get('/', (req, res) => {
