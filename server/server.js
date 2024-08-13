c254a5a9d0909f90fc3ebf5ad162ae72551313d7
Md Saquib Ansari <saquibchiksi786@gmail.com>
2024-08-13 14:13:57 +0530

    commit

--- server/server.js
+++ server/server.js
@@ -27,11 +27,12 @@ app.use(express.static(path.join(__dirname, '..client/build')));
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '..client/build', 'Form.js'));
+  res.sendFile(path.resolve(__dirname, '..client/build', 'index.html'));
 });
 
 app.get('/', (req, res) => {
-  res.send('Server is running');
+  constfilePath=path.join(__dirname, 'Task1', 'public', 'index.html');
+  console.log('Serving index.html from:', filePath);
 });
 
 
