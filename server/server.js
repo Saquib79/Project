8147e08a4aafd2db64723c38041287f768ff8a51
Saquib79 <161459181+Saquib79@users.noreply.github.com>
2024-08-13 14:27:18 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -27,17 +27,7 @@ const path = require('path');
 const app = express();
 
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
-
-// Handle any requests that don't match the ones above
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'));
-});
-
-const PORT = process.env.PORT || 10000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+app.use(express.static(path.join(__dirname, '..client/build')));
 
 
 // The "catchall" handler: for any request that doesn't
